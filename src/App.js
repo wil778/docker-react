680226894becd7be7f92f80b7abcf47a15410462
wil <dev@wilson-wong.com>
2020-02-02 07:15:56 -0800

    fix app text with test case

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          :(
+          :) Changed.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch
+          Learn React
         </a>
       </header>
     </div>
